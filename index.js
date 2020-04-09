19e233903ca22d3d26a692cdbaa60f72855d6aaf
AndreyAlth <andreyalth@gmail.com>
2020-04-08 19:44:06 -0500

    Agregando plantillas para responder por imagen

--- index.js
+++ index.js
@@ -77,10 +77,42 @@ function handleMessage(sender_psid, received_message) {
     let response;
     //check if the message contains text
     if(received_message.text){
-        //create the payload for a basic text message
+        //create the payload for a basic text message, which
+        //will be added to the body of our request to the send API
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me a image!`
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment`
         }
+    }
+    //check if message contains image
+    else if (received_message.attachments) {
+        //Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url
+        response = {
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type": "generic",
+                    "elements":[{
+                        "title": "Is this the right picture?",
+                        "subtitle":"Tap a button to answer.",
+                        "image_url":attachment_url,
+                        "buttons":[
+                            {
+                                "type":"postback",
+                                "title":"Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type":"postback",
+                                "title": "No!",
+                                "payload":"no"
+                            }
+                        ]
+                    }]
+                }
+            }
+        }
+
     }
 
     //send the response message
